78c43da4eba34039c72a5823b1d379906b524fa3
ElNatcho <gebhardsven@gmail.com>
2017-04-21 12:14:47 +0200

    CMazeGenerator: >Grundlegender Generierungs-Algorithmus hinzugefügt: ->_createMazeData() ->_getCellData(int x, int y) ->_getAvailableOptions(int x, int y) ->_generateMaze(int x, int y) ->_mazeData ->_availableOptions ->_curOption

--- MazeGenerator/CMazeGenerator.cpp
+++ MazeGenerator/CMazeGenerator.cpp
@@ -4,6 +4,9 @@
 CMazeGenerator::CMazeGenerator() {
 	// Alloc Memory
 	_mazeSize = new MazeSize();
+	_mazeData = new std::vector<std::vector<BYTE>>();
+	_curOption = new BYTE;
+	_availableOptions = new std::vector<BYTE>();
 
 }
 
@@ -12,18 +15,87 @@ CMazeGenerator::CMazeGenerator() {
 // @param x, y: Neue Größe
 //
 void CMazeGenerator::setMazeSize(int x, int y) {
-
+	_mazeSize->x = x;
+	_mazeSize->y = y;
+	_createMazeData();
 }
 
 // -- generateMaze --
 // Methode erstellt das Labyrinth
 //
 void CMazeGenerator::generateMaze() {
-
+	_generateMaze(0, 0);
 }
+
+// -- resetMazeData --
+// Methode erstellt die _mazeData-Matrix
+//
+void CMazeGenerator::_createMazeData() {
+	_mazeData->clear();
+	for (int y = 0; y < _mazeSize->y; y++) {
+		_mazeData->push_back(std::vector<BYTE>());
+		for (int x = 0; x < _mazeSize->x; x++) {
+			_mazeData->at(y).push_back(0);
+		}
+	}
+}
+
+// -- _getCellData --
+// Methode mit der man sicher auf den Inhalt einer Zelle zugreifen kann
+// @param x, y: Position der Zelle
+//
+int CMazeGenerator::_getCellData(int x, int y) {
+	if (x < 0 || x >= _mazeSize->x ||
+		y < 0 || y >= _mazeSize->y) {
+		return -1;
+	} else {
+		return _mazeData->at(y).at(x);
+	}
+}
+
+// -- _getAvailableOptions --
+// Methode prüft welche Richtungen von einem bestimmten Punkt aus verfügbar sind
+// @param x, y: Punkt
+//
+void CMazeGenerator::_getAvailableOptions(int x, int y) {
+	_availableOptions->clear();
+	if (_getCellData(x + 1, y) == 0)
+		_availableOptions->push_back(PATH_R);
+	if (_getCellData(x - 1, y) == 0)
+		_availableOptions->push_back(PATH_L);
+	if (_getCellData(x, y + 1) == 0)
+		_availableOptions->push_back(PATH_D);
+	if (_getCellData(x, y - 1) == 0)
+		_availableOptions->push_back(PATH_U);
+}
+
+// -- _generateMaze --
+// Methode die ein Labyrinth von einem bestimmten Punkt aus erzeugt
+// @param x, y: Punkt von dem aus gestartet werden soll
+//
+void CMazeGenerator::_generateMaze(int x, int y) {
+	_getAvailableOptions(x, y);
+	while (_availableOptions->size() > 0) {
+		*_curOption = _availableOptions->at(rand() % _availableOptions->size());
+		_mazeData->at(y).at(x) |= *_curOption;
+		if ((*_curOption & PATH_L) >> 3)
+			x--;
+		else if (*_curOption & PATH_R)
+			x++;
+		else if (*_curOption & PATH_U >> 2)
+			y--;
+		else if (*_curOption & PATH_D >> 1)
+			y++;
+		_getAvailableOptions(x, y);
+	}
+}
+
 
 // -- Destruktor --
 CMazeGenerator::~CMazeGenerator() {
 	// Free Memory
 	SAFE_DELETE(_mazeSize);
+	SAFE_DELETE(_mazeData);
+	SAFE_DELETE(_curOption);
+	SAFE_DELETE(_availableOptions);
 }
--- MazeGenerator/CMazeGenerator.hpp
+++ MazeGenerator/CMazeGenerator.hpp
@@ -9,14 +9,12 @@
 #define SAFE_DELETE(x) {if(x!=nullptr){delete(x); x=nullptr;}}
 
 // Path-Flags
-#define PATH_L 0x0008 // Pfad links
-#define PATH_U 0x0004 // Pfad hoch
-#define PATH_D 0x0002 // Pfad runter
-#define PATH_R 0x0001 // Pfad rechts
+#define PATH_L 0x08 // Pfad links
+#define PATH_U 0x04 // Pfad hoch
+#define PATH_D 0x02 // Pfad runter
+#define PATH_R 0x01 // Pfad rechts
 
-#define IS_PATH 0x0010 // Bit das bestimmt ob der Knoten ein Weg ist
-
-#define MAX_ROOM_NUM 0xFFE // Maximale Anzahl der Räume
+typedef unsigned char BYTE;
 
 // CMazeGenerator
 class CMazeGenerator {
@@ -40,6 +38,17 @@ private:
 
 	// -- Member Vars --
 	MazeSize *_mazeSize;
+
+	std::vector<std::vector<BYTE>> *_mazeData;
+
+	std::vector<BYTE> *_availableOptions;
+	BYTE *_curOption;
+
+	// -- Member Methoden --
+	void _createMazeData();
+	int  _getCellData(int x, int y);
+	void _getAvailableOptions(int x, int y);
+	void _generateMaze(int x, int y);
 
 };
 

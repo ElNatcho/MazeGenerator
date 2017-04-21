2476e7ce564b83b4a35ce40fbd3c1f4d9daa13a9
ElNatcho <gebhardsven@gmail.com>
2017-04-21 10:58:56 +0200

    CMazeGenerator: >Grundklasse hinzugefügt

---
+++ MazeGenerator/CMazeGenerator.cpp
@@ -0,0 +1,11 @@
+#include"CMazeGenerator.hpp"
+
+// -- Konstruktor --
+CMazeGenerator::CMazeGenerator() {
+
+}
+
+// -- Destruktor --
+CMazeGenerator::~CMazeGenerator() {
+
+}
---
+++ MazeGenerator/CMazeGenerator.hpp
@@ -0,0 +1,20 @@
+#ifndef CMAZEGENERATOR_HPP
+#define CMAZEGENERATOR_HPP
+
+// Includes
+#include<iostream>
+#include<vector>
+
+// CMazeGenerator
+class CMazeGenerator {
+public:
+
+	// -- CMazeGenerator --
+	CMazeGenerator();
+	~CMazeGenerator();
+
+private:
+
+};
+
+#endif

8258fc5d37a7ee9ce66ab4d774e9c9e6ed271dc7
ElNatcho <gebhardsven@gmail.com>
2017-04-21 11:21:08 +0200

    CMazeGenerator: >Path-Flags wurden hinzugefügt >Struktur MazeSize wurde erstellt >Grundlegende Methoden wurden hinzugefügt ->setMazeSize(x, y) ->generateMaze()

--- MazeGenerator/CMazeGenerator.cpp
+++ MazeGenerator/CMazeGenerator.cpp
@@ -2,10 +2,28 @@
 
 // -- Konstruktor --
 CMazeGenerator::CMazeGenerator() {
+	// Alloc Memory
+	_mazeSize = new MazeSize();
+
+}
+
+// -- setMazeSize --
+// Methode legt die Größe des Labyrinths fest
+// @param x, y: Neue Größe
+//
+void CMazeGenerator::setMazeSize(int x, int y) {
+
+}
+
+// -- generateMaze --
+// Methode erstellt das Labyrinth
+//
+void CMazeGenerator::generateMaze() {
 
 }
 
 // -- Destruktor --
 CMazeGenerator::~CMazeGenerator() {
-
+	// Free Memory
+	SAFE_DELETE(_mazeSize);
 }
--- MazeGenerator/CMazeGenerator.hpp
+++ MazeGenerator/CMazeGenerator.hpp
@@ -5,15 +5,41 @@
 #include<iostream>
 #include<vector>
 
+// SAFE_DELETE Makro
+#define SAFE_DELETE(x) {if(x!=nullptr){delete(x); x=nullptr;}}
+
+// Path-Flags
+#define PATH_L 0x0008 // Pfad links
+#define PATH_U 0x0004 // Pfad hoch
+#define PATH_D 0x0002 // Pfad runter
+#define PATH_R 0x0001 // Pfad rechts
+
+#define IS_PATH 0x0010 // Bit das bestimmt ob der Knoten ein Weg ist
+
+#define MAX_ROOM_NUM 0xFFE // Maximale Anzahl der Räume
+
 // CMazeGenerator
 class CMazeGenerator {
 public:
+
+	// Struktur die die Größe des Labyrinths abspeichert
+	struct MazeSize {
+		int x;
+		int y;
+	};
 
 	// -- CMazeGenerator --
 	CMazeGenerator();
 	~CMazeGenerator();
 
+	// -- Methoden --
+	void setMazeSize(int x, int y);
+	void generateMaze();
+
 private:
+
+	// -- Member Vars --
+	MazeSize *_mazeSize;
 
 };
 

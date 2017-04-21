defbcaf5db35da9495a2ec52ec9bbeb5094445ed
ElNatcho <gebhardsven@gmail.com>
2017-04-21 14:05:29 +0200

    main: >Kommandozeilenparameter hinzugefügt >srand(time(0))

--- MazeGenerator/main.cpp
+++ MazeGenerator/main.cpp
@@ -1,12 +1,19 @@
 #include"CMazeSaver.hpp"
 
-int main() {
+int main(int argc, char **argv) {
+	if (argc != 4) {
+		std::cout << "CMazeSaver.exe <size_x> <size_y> <img_path>" << std::endl;
+		return 0;
+	}
+
+	srand(time(0));
+
 	CMazeGenerator *mazeGenerator = new CMazeGenerator();
 	CMazeSaver	   *mazeSaver	  = new CMazeSaver();
 
-	mazeGenerator->setMazeSize(7, 7);
+	mazeGenerator->setMazeSize(std::stoi(argv[1]), std::stoi(argv[2]));
 	mazeGenerator->generateMaze();
-	mazeSaver->exportMaze("test.png", mazeGenerator->getMaze());
+	mazeSaver->exportMaze(argv[3], mazeGenerator->getMaze());
 
 	return 0;
 }
